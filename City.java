df5b66024952561272745b1341afbd5feda16620
erhanyalniz <45834774+erhanyalniz@users.noreply.github.com>
2021-03-04 18:27:09 +0300

    Add files via upload

---
+++ City.java
@@ -0,0 +1,12 @@
+// Erhan Yalniz   -   150117905
+
+// Simple city class used to represent cities in TSP problem.
+public class City {
+	long x, y;
+	int id;
+	public City(int i, long x0, long y0) {
+		id = i;
+		x = x0;
+		y = y0;
+	}
+}
